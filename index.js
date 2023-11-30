5f3f02d64e934c7919bc477b223b78dc2a1fadfd
Primal <66prg99@gmail.com>
2023-11-30 04:30:09 +0000

    - login/register
    user credentials stuff

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var express = require ('express')
 var ejs = require('ejs')
 var bodyParser= require ('body-parser')
 var mysql = require('mysql');
+var session = require('express-session');
 
 // Create the express application object
 const app = express()
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: false
+}));
+
 const port = 8000
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -41,7 +48,7 @@ app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
 // Define our data
-var forumData = {forumName: "Mango Forums"}
+var forumData = {forumName: "Mango Forums", forumUsername: ""}
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, forumData);
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,84 @@
 module.exports = function(app, forumData) {
 
     // Handle our routes
-    app.get('/',function(req,res){
-        res.render('index.ejs', forumData)
+    app.get('/', (req, res) => {
+        if (req.session && req.session.userId) {
+            forumData.forumUsername = req.session.forumUsername;
+        }
+        res.render('homepage.ejs', forumData);
+    });
+
+    // Handle the login
+    app.get('/login', (req, res) => {
+        res.render('login.ejs', forumData);
+    });
+
+    // Handle the login when submitted
+    app.post('/login', (req, res) => {
+        // Query the DB for any matches
+        let sqlquery = 'SELECT * FROM Users WHERE email = ? AND password = ?';
+        db.query(sqlquery, [req.body.email, req.body.password], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.redirect('/login');
+            }
+    
+            // If the returning password is null or the password doesnt match we prompt the login again
+            if (result.length === 0 || req.body.password !== result[0].password) {
+                return res.redirect('/login');
+            }
+    
+            // If all else is good then we can cache the userID
+            const userId = result[0].id;
+            req.session.userId = userId;
+    
+            // Retrieve the username
+            let sqlquery2 = 'SELECT userName FROM Users WHERE id = ?';
+            db.query(sqlquery2, [userId], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.redirect('/login');
+                }
+                // Store the username in the session
+                req.session.forumUsername = result[0].userName;
+                // Redirect to the addpost page
+                res.redirect('/addpost');
+            });
+        });
+    });
+    
+    // Logout of the account
+    app.post('/logout', (req, res) => {
+        // Clear everything the session gathered and nuke the saved username
+        delete req.session.userId;
+        // Clears the cached forumUsername in both session and index.js var;
+        delete req.session.forumUsername;
+        forumData.forumUsername = "";
+        // Redirect to the homepage to refresh
+        res.redirect('/')
+    });
+
+    
+    app.get('/register', (req, res) => {
+        res.render('register.ejs', forumData);
+    });
+    
+    // Registration Page - POST
+    app.post('/register', (req, res) => {
+        // Insert the new user into the Users table
+        let sqlquery = "INSERT INTO Users (userName, email, password) VALUES (?, ?, ?)";
+        
+        // Assuming that your registration form has fields named 'username', 'email', and 'password'
+        const { username, email, password } = req.body;
+    
+        db.query(sqlquery, [username, email, password], (err, result) => {
+            if (err) {
+                console.error(err);
+                res.redirect('/register'); // Redirect to the registration page in case of an error
+            } else {
+                res.redirect('/login'); // Redirect to the login page after successful registration
+            }
+        });
     });
 
     // About page route
@@ -29,7 +105,6 @@ module.exports = function(app, forumData) {
         });
     });
     
-
     // Show all registered users
     app.get('/users',function(req,res){
         // Get the names of all the users
@@ -65,16 +140,52 @@ module.exports = function(app, forumData) {
         });
     });
 
+    // Add a post
+    app.get('/addpost', function(req, res) {
+        // Check if the user is authenticated by checking the session
+        if (req.session && req.session.userId) {
+            // Get the subscribed topics for the logged-in user
+            let userId = req.session.userId;
+            let sqlquery = "SELECT Topics.* FROM Topics INNER JOIN UserTopics ON Topics.id = UserTopics.TopicID WHERE UserTopics.UserID = " + userId;
+    
+            db.query(sqlquery, [userId], (err, userSubscribedTopics) => {
+                if (err) {
+                    res.redirect('./');
+                } else {
+                    // Render the addpost page with the correct information
+                    res.render("addpost.ejs", { topicsData: userSubscribedTopics });
+                }
+            });
+        } else {
+            // If the user isnt authenticated, then get them to login
+            res.redirect('/login');
+        }
+    });
+
+    // Try to upate the DB when adding a post
+    app.post('/addedpost', function(req, res) {
+        // Insert the new post into the Posts table
+        let sqlquery = "INSERT INTO Posts (description, TopicID, UserID) VALUES (?, ?, ?)";
+        db.query(sqlquery, [req.body.description, req.body.topic, req.session.userId], (err, result) => {
+            if (err) {
+                res.redirect('./'); 
+            } else {
+                res.redirect('./'); 
+            }
+        });
+    });
+
+    // Route to page where user can specify keyword to search for
     app.get('/search',function(req,res){
         res.render("searchposts.ejs", forumData);
     });
 
-
+    // Keyword based query for a return page with the list of matching posts
     app.get('/searchedpostsresult', function (req, res) {
         // Query to find posts that contain the specified keyword
         let sqlquery = "SELECT Posts.*, Users.id AS creatorID, Users.userName AS creatorUsername, Topics.topicName FROM Posts INNER JOIN Users ON Posts.UserID = Users.id INNER JOIN Topics ON Posts.TopicID = Topics.id WHERE Posts.description LIKE ?";
         let keyword = '%' + req.query.keyword + '%';
-        
+
         // execute sql query
         db.query(sqlquery, [keyword], (err, result) => {
             if (err) {
@@ -89,40 +200,4 @@ module.exports = function(app, forumData) {
             }
         });
     });
-    
-    
-    
-
-    app.get('/addpost', function(req, res) {
-        // TODO get userID from login and use that here
-        let userId = 1;
-    
-        // Get the subscribed topics for the logged in user
-        let sqlquery = "SELECT Topics.* FROM Topics INNER JOIN UserTopics ON Topics.id = UserTopics.TopicID WHERE UserTopics.UserID = " + userId;
-        
-        db.query(sqlquery, [userId], (err, userSubscribedTopics) => {
-            if (err) {
-                res.redirect('./'); 
-            } else {
-                // Render the addpost page with the correct information
-                res.render("addpost.ejs", { topicsData: userSubscribedTopics });
-            }
-        });
-    });
-
-    app.post('/addedpost', function(req, res) {
-        // Insert the new post into the Posts table
-        let sqlquery = "INSERT INTO Posts (description, TopicID, UserID) VALUES (?, ?, ?)";
-        // TODO get the userID thats trying to post
-        db.query(sqlquery, [req.body.description, req.body.topic, 1], (err, result) => {
-            if (err) {
-                res.redirect('./'); 
-            } else {
-                res.redirect('./'); 
-            }
-        });
-    });
-    
-    
-
 }
