78e4c83d3a58a224fd7d9247525195195d7d5357
Primal <66prg99@gmail.com>
2023-12-04 02:53:33 +0000

    - Lots of updates
    - Initial Styling pass
    - Following Topics
    - Login/Register working in coordination with topics and login redirects to homepage

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
 // Import the modules we need
-var express = require ('express')
-var ejs = require('ejs')
-var bodyParser= require ('body-parser')
-var mysql = require('mysql');
-var session = require('express-session');
+var express = require("express");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var session = require("express-session");
 
 // Create the express application object
-const app = express()
-app.use(session({
-    secret: 'your_secret_key',
+const app = express();
+app.use(
+  session({
+    secret: "your_secret_key",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 
-const port = 8000
-app.use(bodyParser.urlencoded({ extended: true }))
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define the database connection
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'appuser',
-    password: 'app2027',
-    database: 'mangoForums'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "appuser",
+  password: "app2027",
+  database: "mangoForums",
 });
 // Connect to the database
 db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('Connected to database');
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database");
 });
 global.db = db;
 
-
 // Set up css
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Set the directory where Express will pick up HTML files
 // __dirname will get the current directory
-app.set('views', __dirname + '/views');
+app.set("views", __dirname + "/views");
 
 // Tell Express that we want to use EJS as the templating engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Tells Express how we should process html files
 // We want to use EJS's rendering engine
-app.engine('html', ejs.renderFile);
+app.engine("html", ejs.renderFile);
 
 // Define our data
-var forumData = {forumName: "Mango Forums", forumUsername: ""}
+var forumData = { forumName: "Mango Forums", forumUsername: "" };
 
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./routes/main")(app, forumData);
 
 // Start the web app listening
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/main.js
+++ routes/main.js
@@ -1,203 +1,265 @@
-module.exports = function(app, forumData) {
+module.exports = function (app, forumData) {
+  // Handle our routes
+  app.get("/", (req, res) => {
+    if (req.session && req.session.userId) {
+      forumData.forumUsername = req.session.forumUsername;
+    }
+    res.render("homepage.ejs", forumData);
+  });
 
-    // Handle our routes
-    app.get('/', (req, res) => {
-        if (req.session && req.session.userId) {
-            forumData.forumUsername = req.session.forumUsername;
+  // Handle the login
+  app.get("/login", (req, res) => {
+    res.render("login.ejs", forumData);
+  });
+
+  // Handle the login when submitted
+  app.post("/login", (req, res) => {
+    // Query the DB for any matches
+    let sqlquery = "SELECT * FROM Users WHERE email = ? AND password = ?";
+    db.query(sqlquery, [req.body.email, req.body.password], (err, result) => {
+      if (err) {
+        console.error(err);
+        return res.redirect("/login");
+      }
+
+      // If the returning password is null or the password doesnt match we prompt the login again
+      if (result.length === 0 || req.body.password !== result[0].password) {
+        return res.redirect("/login");
+      }
+
+      // If all else is good then we can cache the userID
+      const userId = result[0].id;
+      req.session.userId = userId;
+
+      // Retrieve the username
+      let sqlquery2 = "SELECT userName FROM Users WHERE id = ?";
+      db.query(sqlquery2, [userId], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.redirect("/login");
         }
-        res.render('homepage.ejs', forumData);
+        // Store the username in the session
+        req.session.forumUsername = result[0].userName;
+        // Redirect to the homepage
+        res.redirect("./");
+      });
     });
+  });
 
-    // Handle the login
-    app.get('/login', (req, res) => {
-        res.render('login.ejs', forumData);
+  // Logout of the account
+  app.post("/logout", (req, res) => {
+    // Clear everything the session gathered and nuke the saved username
+    delete req.session.userId;
+    // Clears the cached forumUsername in both session and index.js var;
+    delete req.session.forumUsername;
+    forumData.forumUsername = "";
+    // Redirect to the homepage to refresh
+    res.redirect("/");
+  });
+
+  app.get("/register", (req, res) => {
+    res.render("register.ejs", forumData);
+  });
+
+  // Registration Page - POST
+  app.post("/register", (req, res) => {
+    // Insert the new user into the Users table
+    let sqlquery =
+      "INSERT INTO Users (userName, email, password) VALUES (?, ?, ?)";
+
+    // Assuming that your registration form has fields named 'username', 'email', and 'password'
+    const { username, email, password } = req.body;
+
+    db.query(sqlquery, [username, email, password], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.redirect("/register"); // Redirect to the registration page in case of an error
+      } else {
+        res.redirect("/login"); // Redirect to the login page after successful registration
+      }
     });
+  });
 
-    // Handle the login when submitted
-    app.post('/login', (req, res) => {
-        // Query the DB for any matches
-        let sqlquery = 'SELECT * FROM Users WHERE email = ? AND password = ?';
-        db.query(sqlquery, [req.body.email, req.body.password], (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.redirect('/login');
-            }
-    
-            // If the returning password is null or the password doesnt match we prompt the login again
-            if (result.length === 0 || req.body.password !== result[0].password) {
-                return res.redirect('/login');
-            }
-    
-            // If all else is good then we can cache the userID
-            const userId = result[0].id;
-            req.session.userId = userId;
-    
-            // Retrieve the username
-            let sqlquery2 = 'SELECT userName FROM Users WHERE id = ?';
-            db.query(sqlquery2, [userId], (err, result) => {
-                if (err) {
-                    console.error(err);
-                    return res.redirect('/login');
-                }
-                // Store the username in the session
-                req.session.forumUsername = result[0].userName;
-                // Redirect to the addpost page
-                res.redirect('/addpost');
-            });
+  // About page route
+  app.get("/about", function (req, res) {
+    res.render("about.ejs", forumData);
+  });
+
+  // Show all the topics
+  app.get("/topics", function (req, res) {
+    // Get information about all topics and their creators
+    let sqlquery =
+      "SELECT Topics.*, Users.userName AS creatorUsername FROM Topics INNER JOIN Users ON Topics.creatorUserID = Users.id";
+
+    // Create a blacklist array, we will fill this with topics the user already follows
+    var blacklist = " ";
+
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+
+      // Check if the user is logged in
+      if (req.session && req.session.userId) {
+        // Fetch topics that the user follows
+        let sqlquery2 =
+          "SELECT * FROM UserTopics JOIN Topics ON UserTopics.TopicID = Topics.id WHERE UserTopics.UserID = ?";
+
+        db.query(sqlquery2, [req.session.userId], (err, followedTopics) => {
+          if (err) {
+            res.redirect("./");
+          }
+
+          // Extract topic names from the list of all topics
+          blacklist = followedTopics.map((topic) => topic.topicName);
+
+          // Extended the forum base data to include the sql result and blacklist
+          let newData = Object.assign({}, forumData, {
+            topicsData: result,
+            blacklist: blacklist,
+          });
+          console.log(blacklist);
+
+          // Render the EJS template with the extended data
+          res.render("topics.ejs", newData);
         });
-    });
-    
-    // Logout of the account
-    app.post('/logout', (req, res) => {
-        // Clear everything the session gathered and nuke the saved username
-        delete req.session.userId;
-        // Clears the cached forumUsername in both session and index.js var;
-        delete req.session.forumUsername;
-        forumData.forumUsername = "";
-        // Redirect to the homepage to refresh
-        res.redirect('/')
-    });
-
-    
-    app.get('/register', (req, res) => {
-        res.render('register.ejs', forumData);
-    });
-    
-    // Registration Page - POST
-    app.post('/register', (req, res) => {
-        // Insert the new user into the Users table
-        let sqlquery = "INSERT INTO Users (userName, email, password) VALUES (?, ?, ?)";
-        
-        // Assuming that your registration form has fields named 'username', 'email', and 'password'
-        const { username, email, password } = req.body;
-    
-        db.query(sqlquery, [username, email, password], (err, result) => {
-            if (err) {
-                console.error(err);
-                res.redirect('/register'); // Redirect to the registration page in case of an error
-            } else {
-                res.redirect('/login'); // Redirect to the login page after successful registration
-            }
+      } else {
+        // User is not logged in,just use an empty blacklist
+        let newData = Object.assign({}, forumData, {
+          topicsData: result,
+          blacklist: blacklist,
         });
+        console.log(newData);
+
+        res.render("topics.ejs", newData);
+      }
     });
+  });
 
-    // About page route
-    app.get('/about',function(req,res){
-        res.render('about.ejs', forumData);
-    }); 
+  //When the follow button is clicked on the follow page
+  app.post("/follow-topic", function (req, res) {
+    // Perform the database update to indicate that the user follows the topic
+    let sqlquery =
+      "INSERT INTO UserTopics (userID, topicID) VALUES (?, ?)";
 
-    // Show all the topics
-    app.get('/topics', function(req, res) {
-        // Get information about all topics and their creators
-        let sqlquery = "SELECT Topics.*, Users.userName AS creatorUsername FROM Topics INNER JOIN Users ON Topics.creatorUserID = Users.id";
-        
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                res.redirect('./');
-            }
-            
-            // Extended the forum base data to include the sql result
-            let newData = Object.assign({}, forumData, { topicsData: result });
-            console.log(newData);
-            
-            res.render("topics.ejs", newData);
-        });
+    console.log(req.body.topic);
+    db.query(sqlquery, [req.session.userId, req.body.topic], (err, result) => {
+      if (err) {
+        console.log(err);
+        res.redirect("./");
+      } else {
+        // Redirect to the topics page after successful follow
+        res.redirect("/topics");
+      }
     });
-    
-    // Show all registered users
-    app.get('/users',function(req,res){
-        // Get the names of all the users
-        let sqlquery = "SELECT userName FROM Users";
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                res.redirect('./'); 
-            }
-            // Extended the forum base data to include the sql result
-            let newData = Object.assign({}, forumData, {totalUsers:result});
-            console.log(newData)
-            res.render("users.ejs", newData)
-         });                                                                 
-    }); 
+  });
 
-    // Show all posts
-    app.get('/posts', function(req, res) {
-        // Get information about all posts, including post name, creator username, and topic name
-        let sqlquery = "SELECT Posts.*, Users.userName AS creatorUsername, Topics.topicName FROM Posts INNER JOIN Users ON Posts.UserID = Users.id INNER JOIN Topics ON Posts.TopicID = Topics.id";
-    
-        // execute sql query
-        db.query(sqlquery, (err, result) => {
-            if (err) {
-                res.redirect('./');
-            }
-    
-            // Extended the forum base data to include the sql result
-            let newData = Object.assign({}, forumData, { postsData: result });
-            console.log(newData);
-    
-            res.render("posts.ejs", newData);
-        });
+  // Show all registered users
+  app.get("/users", function (req, res) {
+    // Get the names of all the users
+    let sqlquery = "SELECT userName FROM Users";
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+      // Extended the forum base data to include the sql result
+      let newData = Object.assign({}, forumData, { totalUsers: result });
+      console.log(newData);
+      res.render("users.ejs", newData);
     });
+  });
 
-    // Add a post
-    app.get('/addpost', function(req, res) {
-        // Check if the user is authenticated by checking the session
-        if (req.session && req.session.userId) {
-            // Get the subscribed topics for the logged-in user
-            let userId = req.session.userId;
-            let sqlquery = "SELECT Topics.* FROM Topics INNER JOIN UserTopics ON Topics.id = UserTopics.TopicID WHERE UserTopics.UserID = " + userId;
-    
-            db.query(sqlquery, [userId], (err, userSubscribedTopics) => {
-                if (err) {
-                    res.redirect('./');
-                } else {
-                    // Render the addpost page with the correct information
-                    res.render("addpost.ejs", { topicsData: userSubscribedTopics });
-                }
-            });
+  // Show all posts
+  app.get("/posts", function (req, res) {
+    // Get information about all posts, including post name, creator username, and topic name
+    let sqlquery =
+      "SELECT Posts.*, Users.userName AS creatorUsername, Topics.topicName FROM Posts INNER JOIN Users ON Posts.UserID = Users.id INNER JOIN Topics ON Posts.TopicID = Topics.id";
+
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+
+      // Extended the forum base data to include the sql result
+      let newData = Object.assign({}, forumData, { postsData: result });
+      console.log(newData);
+
+      res.render("posts.ejs", newData);
+    });
+  });
+
+  // Add a post
+  app.get("/addpost", function (req, res) {
+    // Check if the user is authenticated by checking the session
+    if (req.session && req.session.userId) {
+      // Get the subscribed topics for the logged-in user
+      let userId = req.session.userId;
+      let sqlquery =
+        "SELECT Topics.* FROM Topics INNER JOIN UserTopics ON Topics.id = UserTopics.TopicID WHERE UserTopics.UserID = " +
+        userId;
+
+      db.query(sqlquery, [userId], (err, userSubscribedTopics) => {
+        if (err) {
+          res.redirect("./");
         } else {
-            // If the user isnt authenticated, then get them to login
-            res.redirect('/login');
+          // Render the addpost page with the correct information
+          res.render("addpost.ejs", { topicsData: userSubscribedTopics });
         }
-    });
+      });
+    } else {
+      // If the user isnt authenticated, then get them to login
+      res.redirect("/login");
+    }
+  });
 
-    // Try to upate the DB when adding a post
-    app.post('/addedpost', function(req, res) {
-        // Insert the new post into the Posts table
-        let sqlquery = "INSERT INTO Posts (description, TopicID, UserID) VALUES (?, ?, ?)";
-        db.query(sqlquery, [req.body.description, req.body.topic, req.session.userId], (err, result) => {
-            if (err) {
-                res.redirect('./'); 
-            } else {
-                res.redirect('./'); 
-            }
+  // Try to upate the DB when adding a post
+  app.post("/addedpost", function (req, res) {
+    // Insert the new post into the Posts table
+    let sqlquery =
+      "INSERT INTO Posts (description, TopicID, UserID) VALUES (?, ?, ?)";
+    db.query(
+      sqlquery,
+      [req.body.description, req.body.topic, req.session.userId],
+      (err, result) => {
+        if (err) {
+          res.redirect("./");
+        } else {
+          res.redirect("./");
+        }
+      }
+    );
+  });
+
+  // Route to page where user can specify keyword to search for
+  app.get("/search", function (req, res) {
+    res.render("searchposts.ejs", forumData);
+  });
+
+  // Keyword based query for a return page with the list of matching posts
+  app.get("/searchedpostsresult", function (req, res) {
+    // Query to find posts that contain the specified keyword
+    let sqlquery =
+      "SELECT Posts.*, Users.id AS creatorID, Users.userName AS creatorUsername, Topics.topicName FROM Posts INNER JOIN Users ON Posts.UserID = Users.id INNER JOIN Topics ON Posts.TopicID = Topics.id WHERE Posts.description LIKE ?";
+    let keyword = "%" + req.query.keyword + "%";
+
+    // execute sql query
+    db.query(sqlquery, [keyword], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.redirect("./");
+      } else {
+        // Merge the result into the forumdata so that it can be passed into the EJS file
+        // Extended the new data to pass the 'keyword' in so that the result page can show that with the matching list
+        let newData = Object.assign({}, forumData, {
+          postsData: result,
+          query: req.query.keyword,
         });
+        console.log(newData);
+        res.render("searchedpostsresult.ejs", newData);
+      }
     });
-
-    // Route to page where user can specify keyword to search for
-    app.get('/search',function(req,res){
-        res.render("searchposts.ejs", forumData);
-    });
-
-    // Keyword based query for a return page with the list of matching posts
-    app.get('/searchedpostsresult', function (req, res) {
-        // Query to find posts that contain the specified keyword
-        let sqlquery = "SELECT Posts.*, Users.id AS creatorID, Users.userName AS creatorUsername, Topics.topicName FROM Posts INNER JOIN Users ON Posts.UserID = Users.id INNER JOIN Topics ON Posts.TopicID = Topics.id WHERE Posts.description LIKE ?";
-        let keyword = '%' + req.query.keyword + '%';
-
-        // execute sql query
-        db.query(sqlquery, [keyword], (err, result) => {
-            if (err) {
-                console.error(err);
-                res.redirect('./');
-            } else {
-                // Merge the result into the forumdata so that it can be passed into the EJS file
-                // Extended the new data to pass the 'keyword' in so that the result page can show that with the matching list
-                let newData = Object.assign({}, forumData, { postsData: result, query: req.query.keyword });
-                console.log(newData);
-                res.render("searchedpostsresult.ejs", newData);
-            }
-        });
-    });
-}
+  });
+};
